ce0927c1bb5142467d132a594b11b4fd757b3090
masarakki <masaki@hisme.net>
2014-03-24 18:37:56 +0900

    delegate special methods

--- lib/namlet/wrapper.rb
+++ lib/namlet/wrapper.rb
@@ -1,11 +1,15 @@
+require 'forwardable'
+
 module Namlet
   class Wrapper < SimpleDelegator
+    extend Forwardable
     def initialize(name, actual)
       super(actual)
       @name = name
       @actual = actual
     end
 
+    def_delegators :@actual, :class, :is_a?, :kind_of?, :nil?
     def inspect ; @name ; end
   end
 end
